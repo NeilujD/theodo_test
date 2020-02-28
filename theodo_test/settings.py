9f3a42e7f0c510ce6fb85d62c5d2ffa5522dfc64
Julien Collard <julien.collard14@gmail.com>
2020-02-28 11:52:02 +0100

    install and setup django-rest-framework package

--- theodo_test/settings.py
+++ theodo_test/settings.py
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'api'
+    'api',
+
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Django Rest framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissions'
+    ]
+}
