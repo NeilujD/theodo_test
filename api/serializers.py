da025d035583c67596310b3f06d288ea1f96a3e9
Julien Collard <julien.collard14@gmail.com>
2020-02-28 19:10:55 +0100

    add comments

--- api/serializers.py
+++ api/serializers.py
@@ -14,17 +14,15 @@ class RunSerializer(serializers.HyperlinkedModelSerializer):
         fields = ['start_date', 'end_date', 'distance', 'burnt_calories', 'runner']
 
     def validate(self, data):
-        """
-            Check that start_date is before end_date.
-        """
-
         start_date = data.get('start_date')
         end_date = data.get('end_date')
         runner = data.get('runner')
 
+        # check that start_date is before end_date
         if start_date > end_date:
             raise serializers.ValidationError("`end_date` must occur after `start_date`")
 
+        # check that start_date is after the last run end_date
         runs = Run.objects.filter(end_date__gte=start_date, runner=runner)
         if len(runs) > 0:
             raise serializers.ValidationError("new user run have to start after the last one")
